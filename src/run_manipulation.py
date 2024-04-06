e9f5166d8813886d719f3221f95adbe8f8683222
wf34 <wf34@users.noreply.github.com>
2024-04-06 15:37:30 +0300

    inv-kin controller coming together

--- src/run_manipulation.py
+++ src/run_manipulation.py
@@ -2,20 +2,18 @@
 
 import os
 import sys
+import time
 
 from typing import Literal
 
 import numpy as np
 from tap import Tap
-
-from pydrake.geometry import Meshcat
-
-TIME_STEP=0.007  #faster
-IIWA_DEFAULT_POSITION = [-1.57, 0.1, 0, -1.2, 0, 1.6, 0]
+import pydot
 
 from pydrake.all import (
     AddMultibodyPlantSceneGraph,
     DiagramBuilder,
+    Meshcat,
     MeshcatVisualizer,
     MultibodyPlant,
     Simulator,
@@ -30,7 +28,22 @@ from pydrake.all import (
     RevoluteJoint,
     RigidTransform,
     RollPitchYaw,
+    RotationMatrix,
+
+    Adder,
+    Demultiplexer,
+    PassThrough,
+    InverseDynamicsController,
 )
+
+from differential_controller import create_differential_controller
+
+
+TIME_STEP=0.007  #faster
+IIWA_DEFAULT_POSITION = [-1.57, 0.1, 0, -1.2, 0, 1.6, 0]
+
+def FindResource(filename):
+    return os.path.join(os.path.dirname(__file__), filename)
 
 def AddWsg(plant,
            iiwa_model_instance,
@@ -79,30 +92,122 @@ def AddIiwa(plant, collision_model="no_collision"):
 
     return iiwa
 
-def get_manipuland_resource_path(resource_name: str) -> str:
+
+def get_resource_path(resource_name: str) -> str:
     resource_file = os.path.join('resources', f'{resource_name}.sdf')
     full_path = os.path.join(os.path.split(os.path.abspath(os.path.dirname(sys.argv[0])))[0], resource_file)
     if not os.path.exists(full_path) or not os.path.isfile(full_path):
         raise Exception(f'a resource {resource_name} is absent')
     return full_path
 
+
+def get_resource(plant: MultibodyPlant, resource_name: str):
+    resource_path = get_resource_path(resource_name)
+    return Parser(plant=plant).AddModelFromFile(resource_path)
+
+
+def create_iiwa_position_measured_port(builder, plant, iiwa):
+    num_iiwa_positions = plant.num_positions(iiwa)
+    iiwa_output_state = plant.get_state_output_port(iiwa)
+    demux = builder.AddSystem(Demultiplexer(size=num_iiwa_positions*2,
+                                            output_ports_size=num_iiwa_positions))
+    builder.Connect(plant.get_state_output_port(iiwa),
+                    demux.get_input_port())
+    builder.ExportOutput(demux.get_output_port(0), "iiwa_position_measured")
+
+    controller_plant = MultibodyPlant(time_step=TIME_STEP)
+    controller_iiwa = AddIiwa(controller_plant, collision_model="with_box_collision")
+    AddWsg(controller_plant, controller_iiwa, welded=True, sphere=True)
+    controller_plant.Finalize()
+
+    iiwa_controller = builder.AddSystem(
+                InverseDynamicsController(controller_plant,
+                                          kp=[100] * num_iiwa_positions,
+                                          ki=[1] * num_iiwa_positions,
+                                          kd=[20] * num_iiwa_positions,
+                                          has_reference_acceleration=False))
+    iiwa_controller.set_name("iiwa_controller")
+    builder.Connect(plant.get_state_output_port(iiwa),
+                    iiwa_controller.get_input_port_estimated_state())
+
+    adder = builder.AddSystem(Adder(2, num_iiwa_positions))
+    builder.Connect(iiwa_controller.get_output_port_control(),
+                    adder.get_input_port(0))
+    torque_passthrough = builder.AddSystem(PassThrough([0] * num_iiwa_positions))
+    builder.Connect(torque_passthrough.get_output_port(),
+                    adder.get_input_port(1))
+    builder.ExportInput(torque_passthrough.get_input_port(), "iiwa_feedforward_torque")
+    builder.Connect(adder.get_output_port(),
+                    plant.get_actuation_input_port(iiwa))
+
+    return demux.get_output_port(0), iiwa_controller, iiwa_output_state
+
+
 def run_manipulation(method: str):
     meshcat = Meshcat()
     builder = DiagramBuilder()
     plant, scene_graph = AddMultibodyPlantSceneGraph(builder, time_step=TIME_STEP)
     iiwa = AddIiwa(plant, collision_model="with_box_collision")
-    wsg = AddWsg(plant, iiwa, welded=False, sphere=False)
+    #wsg = AddWsg(plant, iiwa, welded=False, sphere=False)
+
+    shelves = get_resource(plant, 'shelves')
+    X_WS = RigidTransform(RotationMatrix.Identity(), [0.5, 0., 0.4085])
+    plant.WeldFrames(
+            plant.world_frame(),
+            plant.GetFrameByName('shelves_body', shelves),
+            X_WS)
+    brick_model = get_resource(plant, 'foam_brick')
+    brick_body = plant.GetBodyByName("base_link", brick_model)
+    X_WB = RigidTransform(RotationMatrix.Identity(), [0.4, 0., 0.4085 + 0.15])
+    plant.SetDefaultFreeBodyPose(brick_body, X_WB)
+    plant.Finalize()
+
+    measured_iiwa_position_port, iiwa_pid_controller, measured_iiwa_state_port = \
+        create_iiwa_position_measured_port(
+            builder, plant, iiwa)
+
+    #########
+    temp_plant_context = plant.CreateDefaultContext()
+    X_G = {"initial": plant.EvalBodyPoseInWorld(temp_plant_context, plant.GetBodyByName("body"))}
+    X_O = {"initial": plant.EvalBodyPoseInWorld(temp_plant_context, plant.GetBodyByName("nut"))}
+    X_OinitialOgoal = RigidTransform(RotationMatrix.MakeZRotation(-np.pi / 6))
+    X_O['goal'] = X_O['initial'].multiply(X_OinitialOgoal)
+    X_G, times = diff2_c.make_gripper_frames(X_G, X_O)
+
+    #########
+    output_iiwa_position_port, output_wsg_position_port, integrator = \
+        create_differential_controller(builder, plant,
+                                       measured_iiwa_position_port,
+                                       X_G, times)
+    #########
+
+    visualizer = MeshcatVisualizer.AddToBuilder(builder, scene_graph, meshcat)
+    diagram = builder.Build()
+    diagram.set_name(sys.argv[0])
+    pydot.graph_from_dot_data(diagram.GetGraphvizString(max_depth=2))[0].write_png('diagram.png')
+
+    simulator = Simulator(diagram)
+    if not simulator:
+        return
+
+    simulator.Initialize()
+    if integrator is not None:
+        integrator.set_integral_value(
+            integrator.GetMyContextFromRoot(simulator.get_mutable_context()),
+                plant.GetPositions(plant.GetMyContextFromRoot(simulator.get_mutable_context()),
+                                   plant.GetModelInstanceByName("iiwa7")))
 
     web_url = meshcat.web_url()
     print(f'Meshcat is now available at {web_url}')
     os.system(f'xdg-open {web_url}')
 
-    if not simulator:
-        return
-
+    total_time = 10
     print(total_time)
     visualizer.StartRecording(False)
     simulator.AdvanceTo(total_time)
+    visualizer.PublishRecording()
+    time.sleep(30)
+
 
 class ManipulationArgs(Tap):
     method: Literal['inv-kin', 'global']  # Which controller to use
@@ -112,6 +217,4 @@ class ManipulationArgs(Tap):
 
 
 if __name__ == '__main__':
-    print(get_manipuland_resource_path('foam_brick'))
-    exit(0)
     run_manipulation(**ManipulationArgs().parse_args().as_dict())
