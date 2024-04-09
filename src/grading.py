7c61a5cfd4fb667421d2a222fad80ef21530718f
wf34 <wf34@users.noreply.github.com>
2024-04-10 02:24:37 +0300

    adds scoring

--- src/grading.py
+++ src/grading.py
@@ -1,5 +1,7 @@
 import copy
 
+import numpy as np
+
 from pydrake.all import MultibodyPlant, Context
 from resource_loader import BRICK_GOAL_TRANSLATION
 
@@ -11,3 +13,10 @@ def get_start_and_end_positions(plant: MultibodyPlant, context: Context):
     X_O['goal'] = copy.deepcopy(X_O['initial'])
     X_O['goal'].set_translation(BRICK_GOAL_TRANSLATION)
     return X_G, X_O
+
+
+def print_score(plant: MultibodyPlant, context: Context):
+    plant_context = plant.GetMyContextFromRoot(context)
+    p_WB = plant.EvalBodyPoseInWorld(plant_context, plant.GetBodyByName('base_link')).translation()
+    dst = np.linalg.norm(p_WB - BRICK_GOAL_TRANSLATION)
+    print('Distance between the brick and its target: {:.3f}; time taken {:.3f}'.format(dst, plant_context.get_time()))
